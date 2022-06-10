333903c931a92e4c212c575d9b4753a8851faa17
sleeplessOrangeCat <878843768@qq.com>
2022-06-10 08:39:32 +0800

    20200610

--- src/api/fm.js
+++
@@ -1,9 +0,0 @@
-
-import { request } from '@/assets/js/request.js'
-
-// 获取FM
-export function getFMApi () {
-  return request({
-    url: '/personal_fm'
-  }).catch((err) => err.response)
-}
--- src/assets/js/config.js
+++
@@ -1,6 +0,0 @@
-// 歌曲播放模式
-export const playMode = {
-  sequence: 0,
-  loop: 1,
-  random: 2
-}
